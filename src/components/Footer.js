6ed040403ff6d79b8f698540ed2fc163f5c9b06f
sjamescarter <stephenjamescarter@gmail.com>
2023-02-18 22:02:34 -0600

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 const Div = styled.div`
-    background-color: gray;
+    background-color: #EBEBEB;
+    padding: .5em;
 `
 
 function Footer() {
     return (
         <Div>
-            <p>This is the footer.</p>
+            <p>
+                Copyright 2023 Stephen James Carter.
+                <br></br>
+                All rights reserved.
+            </p>
         </Div>
     )
 }

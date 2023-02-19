d6fb971821eab677dfefb5e213d3b1568f5ba3c5
sjamescarter <stephenjamescarter@gmail.com>
2023-02-18 22:51:53 -0600

    create NavBar.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledNavLink = styled(NavLink)`
+    background: #227C9D;
+    border-radius: 2em;
+    color: #EBEBEB;
+    margin: .5em;
+    padding: .75em;
+    text-decoration: none;
+    width: 50px;
+`
 
 function Header() {
     return (
         <div>
             <h1>My IEP Writer</h1>
-            <NavLink>Home</NavLink>
-            <NavLink>Calendar</NavLink>
-            <NavLink>Students</NavLink>
-            <NavLink>Add Student</NavLink>
-            <NavLink>Settings</NavLink>
+            <StyledNavLink to="/" exact>Home</StyledNavLink>
+            <StyledNavLink to="/calendar">Calendar</StyledNavLink>
+            <StyledNavLink to="/students">Students</StyledNavLink>
+            <StyledNavLink to="/students/add">Add Student</StyledNavLink>
+            <StyledNavLink to="/settings">Settings</StyledNavLink>
         </div>
     );
 }

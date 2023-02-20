c619bcacae38113a39f038e7450485843834f10c
sjamescarter <stephenjamescarter@gmail.com>
2023-02-19 23:09:40 -0600

    setup add student form

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -2,7 +2,27 @@ import React from "react";
 
 function AddStudent() {
     return (
-        <h2>Add Student</h2>
+        <div>
+            <h2>Add Student</h2>
+            <form>
+                <label htmlFor="id">Student ID: </label>
+                <input type="number" name="id" />
+                <br></br>
+                <label htmlFor="firstName">First Name: </label>
+                <input type="text" name="firstName" />
+                <br></br>
+                <label htmlFor="lastName">Last Name: </label>
+                <input type="text" name="lastName" />
+                <br></br>
+                <label htmlFor="birthdate">Birthdate: </label>
+                <input type="date" name="birthdate" />
+                <br></br>
+                <label htmlFor="iepDate">IEP Deadline: </label>
+                <input type="date" name="iepDate" />
+                <br></br>
+                <input type="submit" value="Add Student" />
+            </form>
+        </div>
     );
 }
 

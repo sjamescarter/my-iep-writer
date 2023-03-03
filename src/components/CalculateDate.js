097c7833ced5f8418ce6751543ee16a0d3013ab7
sjamescarter <stephenjamescarter@gmail.com>
2023-03-03 00:01:50 -0600

    refactor

--- src/components/CalculateDate.js
+++ src/components/CalculateDate.js
@@ -1,13 +1,9 @@
-import React from "react";
-
 function CalculateDate({ iepDate, days }) {
     const [ year, month, day ] = iepDate.split("-");
     const totalDays = days * 7/5
     const newDeadline = new Date(year, month - 1, day - totalDays);
-
+    
     return (newDeadline.toDateString());
 }
 
 export default CalculateDate;
-
-//  <p>{dateTitle}: {newDeadline.toDateString()}</p>

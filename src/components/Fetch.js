10b97aabdfe1ff5e1d5c6db2c07a9593b466b59f
sjamescarter <stephenjamescarter@gmail.com>
2023-03-02 21:31:11 -0600

    create Fetch.js

---
+++ src/components/Fetch.js
@@ -0,0 +1,31 @@
+const API = "http://localhost:3000"
+
+function getRequest(endpoint, setState) {
+  fetch(API + endpoint)
+  .then(r => r.json())
+  .then(data => setState(data));
+}
+
+function postRequest(endpoint, newData, setState, currentState) {
+  fetch(API + endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newData)
+  })
+  .then(r => r.json())
+  .then(student => setState([...currentState, student]));
+}
+
+function deleteRequest(endpoint, id, setState, currentState) {
+  fetch(API + endpoint + "/" + id, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(setState(currentState.filter(student => student.id !== id)));
+}
+
+export { getRequest, postRequest, deleteRequest };
